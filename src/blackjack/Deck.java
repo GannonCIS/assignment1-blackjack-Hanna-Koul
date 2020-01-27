d6f1e95c36a4ef87f72fba526407a591fef40e79
Daria Anna Koul <koul001@gannon.edu>
2020-01-27 17:31:30 -0500

    complete deck class structure

--- src/blackjack/Deck.java
+++ src/blackjack/Deck.java
@@ -10,5 +10,31 @@ package blackjack;
  * @author gubotdev
  */
 public class Deck {
+    private Card[] myCards = new Card[52];
+    private String[] ranks = {};
+    private String[] suits = {};
+    private int nextCard = 0;
+    
+    public Deck(){
+        
+    } 
+    
+    private void initDeck(){
+        
+    }
+    
+    private void shuffle(){
+    
+    }
+    //printDeck is for testing only
+    
+    public void printDeck(){
+        
+    }
+    
+    public Card dealCard(){
+        return null; //change thiss!!!!!!!!!!!!!!!!!!!!!!!!!!
+    }
+    
     
 }
